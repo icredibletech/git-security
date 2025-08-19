36a939439104746d719626583d9204f073b24e74
berkayy-atas <satis.deste@icredible.com>
2025-08-19 15:31:42 +0300

    feat: added action type input

--- scripts/common/validate_inputs.sh
+++ scripts/common/validate_inputs.sh
@@ -4,4 +4,9 @@ if [[ "$OTP_REQUEST_TYPE" != "MAIL" && "$OTP_REQUEST_TYPE" != "AUTHENTICATOR" ]]
   echo "::error ::Invalid otp_request_type. Must be 'MAIL' or 'AUTHENTICATOR'"
   exit 1
 fi
+if [[ "$ACTION" != "backup" && "$ACTION" != "restore" ]]; then
+  echo "::error ::Invalid otp_request_type. Must be 'backup' or 'restore'"
+  exit 1
+fi
+
 
