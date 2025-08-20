16f68e989e63e284b26aac19bfa9fa206e959267
Berkay ATAŞ <berkay.atas@icredible.com>
2025-08-20 18:13:38 +0300

    chore:code optimize

--- scripts/restore/configure_push.sh
+++ scripts/restore/configure_push.sh
@@ -5,8 +5,8 @@ cd repo-mirror
 TOKEN_TO_USE=${REPOSITORY_RESTORATION_TOKEN:-$GITHUB_DEFAULT_TOKEN}
 [ -z "$REPOSITORY_RESTORATION_TOKEN" ]  && bash $ACTION_PATH/scripts/restore/filter_workflows.sh
 
-git config user.name "iCredible File Security"
-git config user.email "file-security@icredible.com"
+git config user.name "$GIT_USER_NAME"
+git config user.email "$GIT_USER_EMAIL"
 git push --mirror --force "https://x-access-token:$TOKEN_TO_USE@github.com/$GITHUB_REPOSITORY.git"
 
 echo "::notice title=Success!::Repository restored successfully"
