38346c988b5ba37b463cea040c7da636c7100793
berkayy-atas <satis.destek@icredible.com>
2025-08-29 17:02:09 +0300

    fix: correct restore warning message and remove broken link

--- scripts/restore/filter_workflows.sh
+++ scripts/restore/filter_workflows.sh
@@ -4,4 +4,4 @@ cd repo-mirror
 
 sudo apt-get install -y git-filter-repo > /dev/null 2>&1
 git filter-repo --force --path .github/workflows --invert-paths
-echo "::warning title=Information About The Scope Of Restoration::The repository will be restored without the ./.github/workflow directory. If you want to restore this directory, you can find the relevant steps at the following link: https://github.com/berkayy-atas/All-in-One-Repo-Repair-Kit?tab=readme-ov-file#step-1-create-a-personal-access-token"
+echo "::warning title=Information About The Scope Of Restore::The repository will be restored without the ./.github/workflow directory. If you want to restore this directory, you can find the relevant steps at the following link: https://github.com/marketplace/actions/icredible-git-security#-personal-access-token-pat-setup-guide-for-repository-restore"
